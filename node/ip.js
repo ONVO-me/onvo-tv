dbb79774856947da7d0357047f2fc7962bb42acd
Hydra de lerne <abdelhamedmo59@gmail.com>
2024-08-15 18:09:54 +0200

    asd

--- node/ip.js
+++ node/ip.js
@@ -3,10 +3,9 @@ const os = require('os');
 function getNetwork(e) {
     return new Promise((resolve, reject) => {
         const interfaces = os.networkInterfaces();
-        resolve(interfaces);
         for (let interfaceName in interfaces) {
             for (let iface of interfaces[interfaceName]) {
-                if (iface.family === 'IPv4' && !iface.internal && (iface.cidr?.includes('/24') || iface.cidr?.address.includes('192'))) {
+                if (iface.family === 'IPv4' && !iface.internal && (iface.cidr?.includes('/24') || iface.cidr?.address?.includes('192'))) {
                     resolve({ip: iface.address});
                 }
             }
